f24606b55215965426816830709ee812e9c3c750
blackflame576 <pws15548@gmail.com>
2024-05-30 10:44:09 +0300

    Refactoring

--- InstallPackages.py
+++ InstallPackages.py
@@ -210,6 +210,8 @@ class macOS:
             return 502
         command = "brew install jsoncpp sqlite3 sqlite-utils fmt clang-format curl googletest gcc zlib cmake libzip openssl wget boost asio libssh2 unzip zip"
         result = os.system(command)
+        command = "wget https://github.com/madler/zlib/archive/refs/tags/v1.1.2.zip && unzip v1.1.2.zip && cd zlib-1.1.2 && sudo ./configure && sudo make && sudo make install"
+        result = os.system(command)
         check_build_tools = self.checkXcodeCommandLineToolset()
         if check_build_tools == True:
             return result
--- InstallPackages.sh
+++ InstallPackages.sh
@@ -3,5 +3,6 @@
 # Mac OSX
 echo "==> Installing libraries"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install jsoncpp sqlite3 sqlite-utils fmt clang-format curl googletest gcc zlib cmake libzip openssl wget boost asio
+brew install jsoncpp sqlite3 sqlite-utils fmt clang-format curl googletest gcc zlib cmake libzip openssl wget boost asio libssh2 unzip zip
+wget https://github.com/madler/zlib/archive/refs/tags/v1.1.2.zip && unzip v1.1.2.zip && cd zlib-1.1.2 && sudo ./configure && sudo make && sudo make install
 echo "==> Libraries successfully installed"
