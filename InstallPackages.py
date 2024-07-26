0c536602c79eabd5bb71fccde27888133951057f
blackflame576 <pws15548@gmail.com>
2024-07-27 01:48:12 +0300

    Add libpq for macOS

--- InstallPackages.py
+++ InstallPackages.py
@@ -208,7 +208,7 @@ class macOS:
             print(delimiter)
             print("==> Failed to install Homebrew")
             return 502
-        command = "brew install jsoncpp sqlite3 sqlite-utils fmt clang-format curl googletest gcc zlib cmake libzip openssl wget boost asio libssh2 unzip zip"
+        command = "brew install jsoncpp sqlite3 sqlite-utils fmt clang-format curl googletest gcc zlib cmake libzip openssl wget boost asio libssh2 unzip zip libpq"
         result = os.system(command)
         command = "wget https://github.com/madler/zlib/archive/refs/tags/v1.1.2.zip && unzip v1.1.2.zip && cd zlib-1.1.2 && sudo ./configure && sudo make && sudo make install"
         result = os.system(command)
